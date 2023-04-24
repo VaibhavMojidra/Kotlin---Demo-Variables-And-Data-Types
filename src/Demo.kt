b03604346620a3cceb45537af408c7987e1f949c
VaibhavMojidra <vmojidra25@gmail.com>
2023-04-25 04:00:46 +0530

    Variables and Data types

---
+++ src/Demo.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+	var a: Int = 10000
+	var d: Double = 100.00
+	var f: Float = 100.00f
+	var l: Long = 1000000004
+	var s: Short = 10
+	var b: Byte = 1
+	var c: Char = 'A'
+	var bol: Boolean = true
+	var str: String = "Vaibhav Mojidra"
+	var numbers: IntArray = intArrayOf(1, 2, 3, 4, 5)
+
+	println("Int Value is " + a)
+	println("Double  Value is " + d)
+	println("Float Value is " + f)
+	println("Long Value is " + l)
+	println("Short Value is " + s)
+	println("Byte Value is " + b)
+	println("Char Value is " + c)
+	println("Boolean Value is " + bol)
+	println("String Value is " + str)
+	println("IntArray Value at 3rd position : " + numbers[2])
+}
